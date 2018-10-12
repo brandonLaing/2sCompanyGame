ab58babf78203ad86c89f0c0144a706f9a9b2f70
BrandonLaing <brandon.robert.laing@gmail.com>
2018-10-11 21:02:12 -0500

    update

--- Assets/Scripts/PlayerControls/PlayerRotator.cs
+++ Assets/Scripts/PlayerControls/PlayerRotator.cs
@@ -9,14 +9,6 @@ public class PlayerRotator : MonoBehaviour
 
   private PlayerStats myStats;
 
-  private float rotationDeadZone = .5F;
-  private float rotationSpeed = 100F;
-
-  public float bodyYPlus;
-  public float bodyYNegitive;
-
-  public float cameraY;
-
   private void Start()
   {
     myStats = GetComponent<PlayerStats>();
@@ -30,48 +22,7 @@ public class PlayerRotator : MonoBehaviour
 
   private void UpdatePlayerRotation()
   {
-    #region Fix cameraEuler
-    float angleEulerLimitBodyY = playerBody.transform.eulerAngles.y;
+    playerBody.eulerAngles = new Vector3(playerBody.eulerAngles.x, playerCamera.eulerAngles.y, playerCamera.eulerAngles.z);
 
-    if (angleEulerLimitBodyY > 180)
-    {
-      angleEulerLimitBodyY -= 360;
-    }
-
-    if (angleEulerLimitBodyY < -180)
-    {
-      angleEulerLimitBodyY += 360;
-    }
-
-    float angleEulerLimitCameraY = playerCamera.transform.eulerAngles.y;
-
-    if (angleEulerLimitCameraY > 180)
-    {
-      angleEulerLimitCameraY -= 360;
-    }
-
-    if (angleEulerLimitCameraY < -180)
-    {
-      angleEulerLimitCameraY += 360;
-    }
-
-    bodyYPlus = angleEulerLimitBodyY + rotationDeadZone;
-    bodyYNegitive = angleEulerLimitBodyY - rotationDeadZone;
-    cameraY = angleEulerLimitCameraY;
-    #endregion
-
-    // check if the body needs to be rotated positive or negitive
-    if (bodyYPlus < cameraY)
-    {
-      Debug.Log("Body needs to rotated negitive");
-      playerBody.eulerAngles += new Vector3(0, rotationSpeed * Time.deltaTime, 0);
-
-    }
-    if (bodyYNegitive > cameraY)
-    {
-      Debug.Log("Body Needs to be rotated positive");
-      playerBody.eulerAngles -= new Vector3(0, rotationSpeed * Time.deltaTime, 0);
-
-    }
   }
 }
