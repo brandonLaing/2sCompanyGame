7b10b909c35e433ed531ec52b95692ba0f1048ce
svc-student <svc-student@student.directory.jccc>
2018-10-31 13:25:14 -0500

    Made health system, worked on mole boss

---
+++ Assets/Scripts/HealthSystem.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthSystem : MonoBehaviour
+{
+  [Header("Health Settings")]
+  public float healthMin;
+  public float healthMax;
+
+  [Space(10)]
+  public float healthPerSecond;
+  public bool onlyInCombat;
+  public bool inCombat;
+
+  [Space(10)]
+  private float _health;
+  public float Health
+  {
+    get { return _health; }
+    set
+    {
+      if (!damageImmune)
+      {
+        if (value < healthMin)
+        {
+          Kill();
+        }
+        if (value > healthMax)
+        {
+          _health = healthMax;
+        }
+
+        _health = value;
+      }
+    }
+  }
+
+  [Header("Other")]
+  public GameObject deathEffect;
+
+  [Header("Debug Features")]
+  private bool damageImmune;
+
+  // heals for that ammount
+  public void Heal(float heal)
+  {
+    Health += heal;
+  }
+
+  // heals to max 
+  public void HealToMax()
+  {
+    Health = healthMax;
+  }
+
+  // damages for ammount
+  public void Damage(float damage)
+  {
+    Health -= damage;
+  }
+
+  // kills objects
+  public virtual void Kill()
+  {
+    if (deathEffect != null)
+    {
+      Instantiate(deathEffect, transform.position, Quaternion.identity);
+    }
+
+    foreach (Transform child in transform)
+    {
+      GameObject.Destroy(child.gameObject);
+    }
+  }
+
+  public void Invincibility(bool godMode)
+  {
+    if (godMode)
+    {
+      damageImmune = true;
+      HealToMax();
+    }
+    else
+    {
+      damageImmune = false;
+    }
+  }
+}
---
+++ Assets/Scripts/HealthSystemEditor.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(HealthSystem))]
+public class HealthSystemEditor : Editor
+{
+  public override void OnInspectorGUI()
+  {
+    DrawDefaultInspector();
+
+    HealthSystem myHealthSyst = (HealthSystem)target;
+    if (GUILayout.Button("Damage"))
+    {
+      myHealthSyst.Damage(1);
+    }
+
+    if (GUILayout.Button("Heal"))
+    {
+      myHealthSyst.Heal(1);
+    }
+
+    if (GUILayout.Button("Heal To Max"))
+    {
+      myHealthSyst.HealToMax();
+    }
+
+    if (GUILayout.Button("God Mode"))
+    {
+      myHealthSyst.Invincibility(true);
+    }
+
+    if (GUILayout.Button("Exit God Mode"))
+    {
+      myHealthSyst.Invincibility(false);
+    }
+
+    if (GUILayout.Button("Kill"))
+    {
+      myHealthSyst.Kill();
+    }
+  }
+}
