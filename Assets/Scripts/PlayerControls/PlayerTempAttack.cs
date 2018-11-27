74b7910184b643d79e825ba05cb0edb0618962c7
Brandon Laing <blaing2@stumail.jccc.edu>
2018-11-27 11:42:45 -0600

    pushing up some stuff

--- Assets/Scripts/PlayerControls/PlayerTempAttack.cs
+++ Assets/Scripts/PlayerControls/PlayerTempAttack.cs
@@ -2,11 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum AttackMode
+{
+  Melee, Ranged
+}
+
 public class PlayerTempAttack : MonoBehaviour {
 
   private PlayerStats ps;
   public Transform playerBody;
   public Transform representation;
+
+  public Transform gunBarrle;
+
+  public AttackMode currentAttackMode;
 
   private void Start()
   {
@@ -16,13 +25,29 @@ public class PlayerTempAttack : MonoBehaviour {
   void Update () {
     if (Input.GetMouseButtonDown(0))
     {
-      Collider[] hitObjects = Physics.OverlapSphere(playerBody.transform.position + transform.forward, 2);
-      foreach (Collider obj in hitObjects)
+      if (currentAttackMode == AttackMode.Melee)
       {
-        IDamageable damageable = obj.GetComponentInParent<IDamageable>();
-        if (damageable != null && damageable != ps)
+        Collider[] hitObjects = Physics.OverlapSphere(playerBody.transform.position + transform.forward, 2);
+        foreach (Collider obj in hitObjects)
         {
-          ps.Attack(damageable, obj.gameObject);
+          IDamageable damageable = obj.GetComponentInParent<IDamageable>();
+          if (damageable != null && damageable != ps)
+          {
+            ps.Attack(damageable, obj.gameObject);
+          }
+        }
+      }
+      else if (currentAttackMode == AttackMode.Ranged)
+      {
+        Ray ray = new Ray(gunBarrle.position, gunBarrle.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 10F ))
+        {
+          if (hit.collider.GetComponentInParent<IDamageable>() != null)
+          {
+            hit.collider.GetComponentInParent<IDamageable>().Damage(10, transform.name);
+          }
         }
       }
     }
