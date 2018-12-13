f266b752b51816629b117fd1965807627f0c1b3c
svc-student <svc-student@student.directory.jccc>
2018-12-12 18:07:33 -0600

    working on persistant

---
+++ Assets/_Scripts/GameSceneManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameSceneManager : MonoBehaviour
+{
+  public string startScene;
+  public string currentScene = string.Empty;
+
+  private Scene scene;
+
+  public delegate void WaitThenDo();
+
+  private Scene ActiveScene
+  {
+    get
+    {
+      return SceneManager.GetSceneByName(currentScene);
+    }
+  }
+
+  private void Start()
+  {
+    if (currentScene == string.Empty)
+    {
+      LoadNewScene(startScene);
+    }
+  }
+
+  public void LoadNewScene(string newScene)
+  {
+    if (currentScene != string.Empty)
+      SceneManager.UnloadSceneAsync(currentScene);
+
+    SceneManager.LoadScene(newScene, LoadSceneMode.Additive);
+
+    currentScene = newScene;
+    Debug.Log(ActiveScene.name);
+
+    WaitThenSetActive(SetSceneActive);
+
+    
+  }
+
+  public void SetSceneActive()
+  {
+
+  }
+
+  public void LoadNewScene(string newScene, Transform player, Vector3 spawnLocation)
+  {
+    player.parent = this.transform;
+
+    LoadNewScene(newScene);
+
+    player.position = spawnLocation;
+    player.parent = null;
+  }
+
+  private IEnumerator WaitThenSetActive(WaitThenDo methodToDo)
+  {
+    yield return new WaitForSeconds(1);
+    methodToDo();
+  }
+}
---
+++ Assets/_Scripts/Generics/LoadNextScene.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadNextScene : MonoBehaviour
+{
+
+  public string sceneToLoad;
+  public float waitTime;
+  public bool loadOnStart;
+
+  private void Start()
+  {
+    if (!loadOnStart)
+      return;
+
+    if (waitTime > 0)
+      StartCoroutine(WaitForNextSceneCorout());
+    else
+      LoadScene(sceneToLoad);
+  }
+
+  private IEnumerator WaitForNextSceneCorout()
+  {
+    yield return new WaitForSeconds(waitTime);
+    LoadScene(sceneToLoad);
+  }
+
+  public void LoadScene(string sceneToLoad)
+  {
+    UnityEngine.SceneManagement.SceneManager.LoadScene(sceneToLoad);
+  }
+}
